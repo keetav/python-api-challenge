62ed382feeff019b293cb7ba77dc76f762af9e59
keetav <71903892+keetav@users.noreply.github.com>
2021-02-08 09:46:43 -0600

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c.acs5.get(('NAME', 'B25034_010E'),
+          {'for': 'state:{}'.format(states.MD.fips)})"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = " AIzaSyDbaj8a9Mdc20fDEuLZB6DuZfwEXZX1tEA"
